ee94c463ba1d765c4dc94804d57492b8fd90c1c7
Tomasz Kargul <75473845+tkargul@users.noreply.github.com>
2023-02-12 16:53:50 +0100

    Update shape.cpp

--- shape.cpp
+++ shape.cpp
@@ -5,13 +5,10 @@ int main()
     int n = 9;
     for (int i = 1; i <= n; i++)
     {
-
         for (int j = 1; j <= n; j++)
         {
-
         //kwadrat
             cout << "* ";
-
             // kwadrat: ramki z przekatnymi
         //if (i == n || i == 1 or j == n or j == 1 or i == j or i + j == n + 1) cout << "* ";
             // dla przekątnych i==j oraz suma i+j jest równa n+1
@@ -23,6 +20,7 @@ int main()
         //if( i+j<n+1 || i+j>n+1) cout<<"* ";
 
         //odkomentować przy wydrukach warunkowych
+            
         //else cout<<"  ";
         
         }
